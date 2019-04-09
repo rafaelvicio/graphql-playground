31dc5fa31133745d4ae1dfaee5a5d9d119cc6170
Rafael Vicio <rafaelvicio@icloud.com>
2019-04-09 00:20:37 -0300

    :sparkles: Job Model

---
+++ src/models/Job.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const JobSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }
+)
+
+export default mongoose.model('jobs', JobSchema);
